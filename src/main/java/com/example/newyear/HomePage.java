abe752ced0866035cbe0afd1a0fbdb3370af9f30
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-12-31 22:37:57 +0400

    v1

--- src/main/java/com/example/newyear/HomePage.java
+++ src/main/java/com/example/newyear/HomePage.java
@@ -1,4 +1,16 @@
 package com.example.newyear;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomePage {
+    @GetMapping
+    public String homePage() {
+        return "index";
+    }
+    @GetMapping("/v1")
+    public String v1Page() {
+        return "v1";
+    }
 }
